778520320138b699bcabb633ab145f8c7f7402a7
danangadita91 <danang.aditarsd@gmail.com>
2018-11-03 12:13:13 +0700

    Perulangan dengan Input nim sebagai batas

---
+++ Soal2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, x, t, batas;
+    cout<<"Masukan nilai n : ";
+    cin>>n;
+    batas = n+ 100;
+    x= 20;
+    t= n;
+    while (t<=batas)
+    {
+        t= t+x;
+        x= x+ 10;
+    }
+    cout<<t;
+    return 0;
+}
